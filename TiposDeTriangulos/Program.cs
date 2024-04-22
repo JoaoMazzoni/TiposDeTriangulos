380df3c91915cb0221b45786ea4d7abfe7e2f6ad
JoaoMazzoni <joao.mazzoni@5by5.com>
2024-04-22 09:08:53 -0300

    Aprimoramento das condições.

--- TiposDeTriangulos/Program.cs
+++ TiposDeTriangulos/Program.cs
@@ -3,41 +3,50 @@
     int lado1 = 0;
     int lado2 = 0;
     int lado3 = 0;
-
     do
     {
-        Console.Write("Digite o lado 1 do triângulo: ");
-        lado1 = int.Parse(Console.ReadLine());
-        if (lado1 == 0 || lado1 < 0)
+        do
         {
-            Console.WriteLine("\n|O lado de um triângulo não pode ser menor ou igual a zero!");
-            Console.WriteLine("-------------------------------------------------------------------\n");
-        }
-    } while (lado1 == 0 || lado1 < 0);
+            Console.Write("Digite o lado 1 do triângulo: ");
+            lado1 = int.Parse(Console.ReadLine());
+            if (lado1 == 0 || lado1 < 0)
+            {
+                Console.WriteLine("\n|O lado de um triângulo não pode ser menor ou igual a zero!");
+                Console.WriteLine("-------------------------------------------------------------------\n");
+            }
+        } while (lado1 == 0 || lado1 < 0);
 
 
-    do
-    {
-        Console.Write("Digite o lado 2 do triângulo: ");
-        lado2 = int.Parse(Console.ReadLine());
-        if (lado2 == 0 || lado2 < 0)
+        do
         {
-            Console.WriteLine("\n|O lado de um triângulo não pode ser menor ou igual a zero!");
-            Console.WriteLine("-------------------------------------------------------------------\n");
-        }
-    } while (lado2 == 0 || lado2 < 0);
+            Console.Write("Digite o lado 2 do triângulo: ");
+            lado2 = int.Parse(Console.ReadLine());
+            if (lado2 == 0 || lado2 < 0)
+            {
+                Console.WriteLine("\n|O lado de um triângulo não pode ser menor ou igual a zero!");
+                Console.WriteLine("-------------------------------------------------------------------\n");
+            }
+        } while (lado2 == 0 || lado2 < 0);
 
 
-    do
-    {
-        Console.Write("Digite o lado 3 do triângulo: ");
-        lado3 = int.Parse(Console.ReadLine());
-        if (lado3 == 0 || lado3 < 0)
+        do
         {
-            Console.WriteLine("\n|O lado de um triângulo não pode ser menor ou igual a zero!");
-            Console.WriteLine("-------------------------------------------------------------------\n");
+            Console.Write("Digite o lado 3 do triângulo: ");
+            lado3 = int.Parse(Console.ReadLine());
+            if (lado3 == 0 || lado3 < 0)
+            {
+                Console.WriteLine("\n|O lado de um triângulo não pode ser menor ou igual a zero!");
+                Console.WriteLine("-------------------------------------------------------------------\n");
+            }
+        } while (lado3 == 0 || lado3 < 0);
+
+        if(lado1 + lado2 < lado3 || lado2 + lado3 < lado1 || lado1 + lado3 < lado2)
+        {
+            Console.WriteLine("\nOs valores informados não formam um triângulo.");
+            Console.WriteLine("----------------------------------------------------\n\n");
         }
-    } while (lado3 == 0 || lado3 < 0);
+
+    } while (lado1 + lado2 < lado3 || lado2 + lado3 < lado1 || lado1 + lado3 < lado2);
 
     if (lado1 == lado2 && lado2 == lado3)
     {
